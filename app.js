9f4608bca2e05862b8f41c3b557097fe20c6fd67
Habil Tabesh <habil.tabesh134@gmail.com>
2022-04-15 20:15:25 -0400

    HomeWork Done

---
+++ app.js
@@ -0,0 +1,49 @@
+//my Variables
+var a = 9;
+var b = 6;
+
+
+//Addition Orginal Function  +
+function addSum(param1, param2){
+    console.log(param1 + param2);}
+addSum(a, b);
+//Addition Arrow Function   +
+const addSum = (param1, param2) => {
+    console.log(param1 + param2);}
+addSum(a,b);
+
+
+
+
+//Subtractraction Original Function   -
+function subtractSum(param1, param2) {
+    console.log(param1 - param2);}
+subtractSum(a,b);
+//Subtractraction Arrow Function   -
+const substractSum =(param1, param2)=> {
+    console.log(param1 - param2);}
+subtractSum(a,b);
+
+
+
+
+//Multiplication Original Function   *
+function multiplySum(param1, param2) {
+    console.log(param1 * param2);}
+multiplySum(a, b);
+
+//Multiplication Arrow function    *
+const multiplysum = (param1, param2) => {
+    console.log(param1 * param2);}
+multiplySum(a, b);
+
+
+
+//Division Original Function   /
+function divideSum(param1, param2) {
+    console.log(param1 / param2);}
+    divideSum(a, b);
+//Divsion Arrow Function  /
+const divideSum = (param1, param2) => {
+    console.log(param1 / param2);}
+    divideSum(a, b);
